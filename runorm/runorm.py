8ebf01f6bb31acdebbc483a9002f34bfbad722c4
violetdenim <119290602+violetdenim@users.noreply.github.com>
2024-11-28 15:57:17 +0300

    Update runorm.py
    bugfix for custom device

--- runorm/runorm.py
+++ runorm/runorm.py
@@ -44,7 +44,7 @@ class RUNorm:
         self.angl_model = T5ForConditionalGeneration.from_pretrained(self.paths["kirillizator"], cache_dir=self.workdir)
         self.tagger_model = BertForTokenClassification.from_pretrained(self.paths["tagger"], cache_dir=self.workdir)
         self.tagger_tokenizer = AutoTokenizer.from_pretrained(self.paths["tagger"], cache_dir=self.workdir)
-        self.tagger = pipeline("ner", model=self.tagger_model, tokenizer=self.tagger_tokenizer, aggregation_strategy="average")
+        self.tagger = pipeline("ner", model=self.tagger_model, tokenizer=self.tagger_tokenizer, aggregation_strategy="average", device=device)
         self.abbr_model.to(device)
         self.angl_model.to(device)
         self.abbr_model.eval()
